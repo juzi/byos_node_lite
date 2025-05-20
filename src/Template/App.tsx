965161947118ec4cfa7f42060c3525064cd0b5a2
Andrii Reinvald <andreinwald@gmail.com>
2025-05-20 13:57:03 +0200

    Added wallpaper

--- src/Template/App.tsx
+++ src/Template/App.tsx
@@ -1,6 +1,7 @@
 import {TemplateData} from "../Data/PrepareData.js";
 import Time from "./Time.js";
 import HackerNews from "./HackerNews.js";
+import Wallpaper from "./Wallpaper.js";
 
 export let templateData: TemplateData;
 
@@ -9,10 +10,13 @@ export default function App(data: TemplateData) {
     return <div style={{
         fontSize: 22,
         display: 'flex',
-        width: '100%',
-        height: '100%',
+        width: '800px',
+        height: '480px',
     }}>
-        <HackerNews style={{left: '30px'}}/>
-        <Time style={{position: 'absolute', top: 8, right: 10}}/>
+        <div style={{display: 'flex', flexDirection: 'column', width: '50%', height: '100%'}}>
+            <Time style={{alignSelf: 'flex-start'}}/>
+            <HackerNews style={{width: '100%', height: '100%'}}/>
+        </div>
+        <Wallpaper style={{width: '50%', height: '100%'}}/>
     </div>
 }
